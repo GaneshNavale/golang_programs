24035b29e8d1f512c2e62cd94c6d0a8a6ada915f
Ganesh Navale <ganesh@edcast.com>
2018-07-11 16:13:05 +0530

    First Go Program 'Two sum'

---
+++ two_sum_01.go
@@ -0,0 +1,33 @@
+/*
+  Problem: Two Sum
+  url: https://leetcode.com/problems/two-sum/description/
+
+  Description: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+  Example.
+    Given nums = [2, 7, 11, 15], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+*/
+
+package main
+import "fmt"
+
+func main() {
+  nums := []int {2, 7, 11, 15}
+  fmt.Println(twoSum(nums, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+  var indices []int
+  length := len(nums)
+  for i := 0; i< length-1; i++ {
+    for j := i+1; j< length; j++ {
+      if (nums[i] + nums[j] == target) {
+        indices = append(indices, i, j)
+      }
+    }
+  }
+  return indices
+}
